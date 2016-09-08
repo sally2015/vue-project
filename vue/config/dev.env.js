eef972a299b0929f48d48f79fb7f6723f1dd7001
sally2015 <740100506@qq.com>
2016-09-08 21:20:18 +0800

    提取路由

--- vue/config/dev.env.js
+++
@@ -1,7 +0,0 @@
-var merge = require('webpack-merge');
-var prodEnv = require('./prod.env');
-//webpack-merge 用于开发环境和生产环境的web pack配置文件的配置合并
-
-module.exports = merge(prodEnv, {
-	NODE_ENV: '"development"'
-});
